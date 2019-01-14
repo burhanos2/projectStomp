5ae5cf5b4cb569b29d3da1c410748cd3fdbc2119
jochemlam <30724588+jochemlam@users.noreply.github.com>
2019-01-14 09:38:33 +0100

    movement finished and tweaked, might need velocity movement instead of transform Translate in the future

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,30 +7,28 @@ public class Movement : MonoBehaviour {
     private InputHandler handler;
     private PlayerInfo playerInfo;
 
+    private readonly float speedMultiplier = 2.5f;
+
     private void Awake()
     {
         handler = new InputHandler();
-        playerInfo = new PlayerInfo();
+        playerInfo = GetComponent<PlayerInfo>();        
     }
 
-    private readonly float speedMultiplier = 5;
+    
 
     private void Update()
     {
-        float x = handler.Xaxis * playerInfo.Speed * speedMultiplier * Time.deltaTime;
-        transform.Translate(x, 0, 0);
-
-        ///////////////////////////////////////////////////////////////////////////////////// de bovenste keuze is raw joystick movement, de onderste keuze is true joystick movement
-        /*
-        if (handler.xAxis < 0)
+        
+        if (handler.Xaxis < 0)
         {
-            transform.Translate(-speed * speedMultiplier * Time.deltaTime, 0, 0);
+            transform.Translate(-playerInfo.Speed * speedMultiplier * Time.deltaTime, 0, 0);
         }
-        if (handler.xAxis > 0)
+        if (handler.Xaxis > 0)
         {
-            transform.Translate(speed * speedMultiplier * Time.deltaTime, 0, 0);
+            transform.Translate(playerInfo.Speed * speedMultiplier * Time.deltaTime, 0, 0);
         }
-        */
+        
     }
     
 }
