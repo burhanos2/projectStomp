1429436463ea9f45cbdeab2757a79467a6cf48ef
Burhan Topaloglu <Burhan@lt20021.local>
2019-01-21 09:39:54 +0100

    hit detect

---
+++ Assets/Scripts/Player/Colliding/HeadCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCheck : MonoBehaviour{
+    private OnHit _onHit;
+
+    private void Awake()
+    {
+        _onHit = new OnHit();
+    }
+
+    // add it to the player head
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "PlayerFeet")
+        {
+            Debug.Log("a foot has hit a head");
+            _onHit.ExecuteDamage();
+        }
+    }
+}
